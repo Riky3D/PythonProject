51d88d1c581835022ed4990340f564b1d032a2e9
Rikesh Dahya <121365929+Riky3D@users.noreply.github.com>
2024-11-08 16:42:40 +0000

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -11,24 +11,12 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 CITY = os.getenv('CITY') 
 
+import json
 
 def lambda_handler(event, context):
-    api_url = f'http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric'
-    response = requests.get(api_url)
-    response.raise_for_status()
-    data = response.json()
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
 
-    weather_data = {
-            'city': CITY,
-            'temperature': data['main']['temp'],
-            'humidity': data['main']['humidity'],
-            'weather': data['weather'][0]['description'],
-            'timestamp': int(datetime.datetime.now().timestamp())
-        }
-    return weather_data
-
-if __name__ == "__main__":
-    weather_data = get_weather_data()
-    print("Weather Data:", weather_data)
-    if weather_data:
-        print("Data fetched successfully.")
