e1f94800d6f944556d15944e19ef86536a3bd4dd
Rikesh Dahya <121365929+Riky3D@users.noreply.github.com>
2024-11-08 16:45:47 +0000

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,16 +1,3 @@
-import boto3
-import requests
-import os
-import datetime
-import json
-from decimal import Decimal
-from dotenv import load_dotenv
-
-load_dotenv()
-
-API_KEY = os.getenv('API_KEY')
-CITY = os.getenv('CITY') 
-
 import json
 
 def lambda_handler(event, context):
